7cd5b438f696a78ed76d5bc882080896ca022d45
nyg <nyg@users.noreply.github.com>
2019-06-08 14:00:19 +0200

    Modification de la config permettant de faire du load balancing.

--- docker-images/apache-reverse-proxy-image/templates/config-template.php
+++ docker-images/apache-reverse-proxy-image/templates/config-template.php
@@ -1,9 +1,28 @@
 <VirtualHost *:80>
+    ProxyRequests Off
     ServerName demo.res.ch
 
-    ProxyPass "/api/addresses/" "http://<?php echo getenv('DYNAMIC_APP'); ?>/"
-    ProxyPassReverse "/api/addresses/" "http://<?php echo getenv('DYNAMIC_APP'); ?>/"
+    <Proxy balancer://dynamic-cluster>
+    <?php
+        $members = preg_split("/,/", getenv('DYNAMIC_NODES'));
+        foreach ($members as $member) {
+            echo "BalancerMember http://$member:3000\n";
+        }
+    ?>
+    </Proxy>
 
-    ProxyPass "/" "http://<?php echo getenv('STATIC_APP'); ?>/"
-    ProxyPassReverse "/" "http://<?php echo getenv('STATIC_APP'); ?>/"
+    <Proxy balancer://static-cluster>
+    <?php
+        $members = preg_split("/,/", getenv('STATIC_NODES'));
+        foreach ($members as $member) {
+            echo "BalancerMember http://$member:80\n";
+        }
+    ?>
+    </Proxy>
+
+    ProxyPass        /api/addresses/ balancer://dynamic-cluster/
+    ProxyPassReverse /api/addresses/ balancer://dynamic-cluster/
+
+    ProxyPass        / balancer://static-cluster/
+    ProxyPassReverse / balancer://static-cluster/
 </VirtualHost>
